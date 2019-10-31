45d1c875a26ef63514e43368722fe5a8f7740f94
Issac Penn <pbbsignup@outlook.com>
2019-10-31 19:30:26 +0800

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Issac Penn on 2019/10/31.
+//  Copyright © 2019 Issac Penn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
